bfa877a34566c3d3fad6993da06459a007bd436e
Winifred Lu <winnie.lu@comcast.net>
2021-11-30 14:28:46 -0500

    mouseover in progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './less/index.less'
 
 // Your code goes here!
+
+// Mouseover
+const introHeader = document.querySelector('.intro');
+const headerP = introHeader.querySelector('p');
+headerP.addEventListener('mouseover', function(){
+    console.log('headerP');
+    headerP.setAttribute('border-style', 'double')
+    headerP.setAttribute('border-color', 'purple');
+    headerP.setAttribute('border-width', '5px');
+});
+
+// Keydown
+//const 
